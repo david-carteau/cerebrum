58f8df08971860378c6252642c4e36f89e775153
david-carteau <carteau.david@gmail.com>
2020-12-01 07:37:22 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# cerebrum
-The Cerebrum library
+## The Cerebrum library
+
+![Logo](/cerebrum/logo.png)
+
+The Cerebrum library can be used to train and use "[NNUE](https://www.chessprogramming.org/NNUE)-like" neural networks for chess engines.
+
+It has been first created for the [Orion UCI chess engine](https://orionchess.pagesperso-orange.fr/).
+
+Helpful information on usage will be found in the source files.
+
+Do not hesitate to send me feedback !
+
+## Copyright, license
+
+Copyright 2020 by David Carteau. All rights reserved.
+
+The Cerebrum libreay is licensed under the MIT License (see "license.txt" file).
---
+++ cerebrum/cerebrum.h
@@ -0,0 +1,121 @@
+/*
+ * The Cerebrum library
+ * Copyright (c) 2020, by David Carteau. All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/****************************************************************************/
+/** NAME : cerebrum.h                                                      **/
+/** AUTHOR : David Carteau, France, December 2020                          **/
+/** LICENSE: MIT (see above and "license.txt" file content)                **/
+/****************************************************************************/
+
+/* 
+ * The library is composed of two parts :
+ * - cerebrum.py : Python program which aiming at training neural networks
+ *   used in the Orion UCI chess engine
+ * - cerebrum.h and cerebrum.c : inference code which can be embedded in a
+ *   chess engine
+ */
+
+#ifndef CEREBRUM_H_INCLUDED
+#define CEREBRUM_H_INCLUDED
+
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <inttypes.h>
+#include "immintrin.h"
+
+
+
+/****************************************************************************/
+/** MACROS                                                                 **/
+/****************************************************************************/
+
+//#define NN_DEBUG
+
+#define NN_FILE "network.nn"
+#define NN_SIZE 128
+
+#define THRESHOLD 3.2f
+#define FACTOR 10000.0f
+
+#define NN_RELU_MIN 0.0f
+#define NN_RELU_MAX 1.0f
+
+
+
+/****************************************************************************/
+/** TYPE DEFINITIONS                                                       **/
+/****************************************************************************/
+
+typedef struct {
+	float W0[40960*NN_SIZE];
+	float B0[NN_SIZE];
+	float W1[NN_SIZE*2*32];
+	float B1[32];
+	float W2[32*32];
+	float B2[32];
+	float W3[32*1];
+	float B3[1];
+} NN_Network;
+
+typedef struct {
+	int16_t W0[40960*NN_SIZE];
+	float B0[NN_SIZE];
+	float W1[NN_SIZE*2*32];
+	float B1[32];
+	float W2[32*32];
+	float B2[32];
+	float W3[32*1];
+	float B3[1];
+} NN_Storage;
+
+/*
+ * pieces must be stored by color and by type, under the bitboard format, with
+ * the following convention:
+ * - white = 0, black = 1
+ * - pawn = index 0, knight = 1, bishop = 2, rook = 3, queen = 4, king = 5
+ */
+
+typedef struct {
+	uint64_t* pieces[2];
+	float accumulator[2][NN_SIZE];
+} NN_Board;
+
+
+
+/****************************************************************************/
+/** EXTERNAL FUNCTIONS                                                     **/
+/****************************************************************************/
+
+int nn_load(NN_Network* nn, char* filename);
+
+void nn_inputs_upd_all(NN_Network* nn, NN_Board* board);
+void nn_inputs_add_piece(NN_Network* nn, NN_Board* board, int piece_type, int piece_color, int piece_position);
+void nn_inputs_del_piece(NN_Network* nn, NN_Board* board, int piece_type, int piece_color, int piece_position);
+void nn_inputs_mov_piece(NN_Network* nn, NN_Board* board, int piece_type, int piece_color, int from, int to);
+
+int nn_eval(NN_Network* nn, NN_Board* board, int color);
+
+#endif // CEREBRUM_H_INCLUDED
